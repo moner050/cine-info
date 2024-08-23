5eb55e8308189a9b5715ae5b82b3bd4a0606ed29
moner050 <minhyung0237@gmail.com>
2024-08-23 18:07:41 +0900

    Feat: Create Cine Info Project

---
+++ src/main/java/com/cineinfo/CineinfoApplication.java
@@ -0,0 +1,13 @@
+package com.cineinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineinfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineinfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cineinfo/CineinfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cineinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineinfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
