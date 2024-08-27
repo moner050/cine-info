a8c24fd6b9a25261123528cbb556e1c6eacbe415
moner050 <minhyung0237@gmail.com>
2024-08-27 12:16:38 +0900

    Feat: 영화인 정보 MoviePeople Entity Builder 패턴 적용, 접근제어 public -> private 수정

--- src/main/java/com/cineinfo/v1/domain/MoviePeople.java
+++ src/main/java/com/cineinfo/v1/domain/MoviePeople.java
@@ -3,15 +3,13 @@ package com.cineinfo.v1.domain;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.annotations.Comment;
+
+import java.util.Objects;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Comment("영화인 정보")
 @Entity(name = "movie_people")
 public class MoviePeople {
@@ -22,22 +20,43 @@ public class MoviePeople {
 
     @Comment("영화인 이름")
     @Column(name = "people_nm", nullable = false, length = 100)
-    public String peopleNm;
+    private String peopleNm;
 
     @Comment("영화인 영어 이름")
     @Column(name = "people_nm_en", length = 100)
-    public String peopleNmEn;
+    private String peopleNmEn;
 
     @Comment("성별")
     @Column(name = "sex", nullable = false, length = 4)
-    public String sex;
+    private String sex;
 
     @Comment("영화인 분류명")
     @Column(name = "rep_role_nm", length = 50)
-    public String repRoleNm;
+    private String repRoleNm;
 
     @Comment("관련 URL")
     @Column(name = "hompages", length = 500)
-    public String hompages;
+    private String hompages;
 
+    @Builder
+    public MoviePeople(String peopleCd, String peopleNm, String peopleNmEn, String sex, String repRoleNm, String hompages) {
+        this.peopleCd = peopleCd;
+        this.peopleNm = peopleNm;
+        this.peopleNmEn = peopleNmEn;
+        this.sex = sex;
+        this.repRoleNm = repRoleNm;
+        this.hompages = hompages;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof MoviePeople moviePeople)) return false;
+        return peopleCd != null && peopleCd.equals(moviePeople.peopleCd);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(peopleCd);
+    }
 }
