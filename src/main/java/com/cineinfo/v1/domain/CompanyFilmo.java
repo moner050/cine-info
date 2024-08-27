8264e7f8e8ed375906da3fd808c1627b77191e00
moner050 <minhyung0237@gmail.com>
2024-08-27 12:15:55 +0900

    Feat: 영화사 필모그래피 CompanyFilmo Entity Builder 패턴 적용

--- src/main/java/com/cineinfo/v1/domain/CompanyFilmo.java
+++ src/main/java/com/cineinfo/v1/domain/CompanyFilmo.java
@@ -1,15 +1,13 @@
 package com.cineinfo.v1.domain;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.annotations.Comment;
+
+import java.util.Objects;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Comment("영화사 필모그래피")
 @Entity(name = "company_filmo")
 public class CompanyFilmo {
@@ -19,10 +17,12 @@ public class CompanyFilmo {
     @Column(name = "id")
     private Long id;
 
+    @Comment("영화사 코드")
     @ManyToOne(optional = false)
     @JoinColumn(name = "company_cd")
     private CompanyInfo companyInfo;
 
+    @Comment("영화코드")
     @ManyToOne(optional = false)
     @JoinColumn(name = "movie_cd")
     private MovieInfo movieInfo;
@@ -34,4 +34,24 @@ public class CompanyFilmo {
     @Comment("영화사 참여 분류명")
     @Column(name = "company_part_nm", length = 100)
     private String companyPartNm;
+
+    @Builder
+    public CompanyFilmo(CompanyInfo companyInfo, MovieInfo movieInfo, String movieNm, String companyPartNm) {
+        this.companyInfo = companyInfo;
+        this.movieInfo = movieInfo;
+        this.movieNm = movieNm;
+        this.companyPartNm = companyPartNm;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof CompanyFilmo companyFilmo)) return false;
+        return id != null && id.equals(companyFilmo.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
