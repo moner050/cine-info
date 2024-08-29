4e509bab2f0c07a692d987e7ffdd3dd47f291148
moner050 <minhyung0237@gmail.com>
2024-08-29 20:39:46 +0900

    Feat: 공통코드 저장 서비스 메서드 추가, 영화 리스트 저장 메서드 테스트 동안 1페이지만 저장되도록 수정.

--- src/main/java/com/cineinfo/v1/service/KOFICApiService.java
+++ src/main/java/com/cineinfo/v1/service/KOFICApiService.java
@@ -1,8 +1,11 @@
 package com.cineinfo.v1.service;
 
 import com.cineinfo.v1.client.KOFICClient;
+import com.cineinfo.v1.domain.kofic.ComCode;
 import com.cineinfo.v1.domain.kofic.MovieInfo;
+import com.cineinfo.v1.dto.kofic.response.SearchCodeListRes;
 import com.cineinfo.v1.dto.kofic.response.SearchMovieListRes;
+import com.cineinfo.v1.dto.kofic.response.comcode.CodesRes;
 import com.cineinfo.v1.dto.kofic.response.movie_list.MovieListRes;
 import com.cineinfo.v1.repository.kofic.*;
 import lombok.RequiredArgsConstructor;
@@ -27,22 +30,52 @@ public class KOFICApiService {
     private final MoviePeopleRepository moviePeopleRepository;
     private final PeopleFilmoRepository peopleFilmoRepository;
 
+    // 공통 코드 저장
+    @Transactional
+    public boolean saveComCode(String summary) {
+        int count = 0;
+        SearchCodeListRes searchCodeList = koficClient.searchCodeList(summary);
+        List<CodesRes> codes = searchCodeList.getCodes();
+
+        for (CodesRes code : codes) {
+            ComCode entity = CodesRes.toEntity(code, summary);
+
+            if (!comCodeRepository.existsById(entity.getFullCd())) {
+                comCodeRepository.save(entity);
+                count++;
+            }
+        }
+
+        log.info(count + " 개 저장 완료.");
+        return true;
+    }
+
     // 영화 리스트 저장
     @Transactional
     public boolean saveMovieList(String curPage, String openStartDt) {
-        SearchMovieListRes searchMovieList = koficClient.searchMovieList(curPage, openStartDt);
-        List<MovieListRes> movieListRes = searchMovieList.getMovieListResult().getMovieList();
+        int count = 0;
+        //! 테스트 끝나면 모든 데이터 넣어줄 예정.
+//        int totCnt = getMovieListTotCnt(openStartDt);
 
-        Long totCnt = searchMovieList.getMovieListResult().getTotCnt();
+//        for (int i = 1; i <= totCnt; i++) {
+            SearchMovieListRes searchMovieList = koficClient.searchMovieList(curPage, openStartDt);
+            List<MovieListRes> movieListRes = searchMovieList.getMovieListResult().getMovieList();
 
-        for (int i = 0; i < movieListRes.size(); i++) {
-            MovieInfo entity = MovieListRes.toEntity(movieListRes.get(i));
+            for (MovieListRes movieListRe : movieListRes) {
+                MovieInfo entity = MovieListRes.toEntity(movieListRe);
 
-            movieInfoRepository.save(entity);
-
-            log.info(i+1 + " 개 저장 완료. 영화코드: " + entity.getMovieCd() + " directors: " + entity.getDirectorPeopleNms());
-        }
-
+                if (!movieInfoRepository.existsById(entity.getMovieCd())) {
+                    movieInfoRepository.save(entity);
+                    count++;
+                }
+            }
+//        }
+        log.info(count + " 개 저장 완료.");
         return true;
+    }
+
+    public int getMovieListTotCnt(String openStartDt) {
+        SearchMovieListRes searchMovieList = koficClient.searchMovieList("1", openStartDt);
+        return searchMovieList.getMovieListResult().getTotCnt();
     }
 }
