8b415819029845f33d46d8aed45a4b9889e17c21
moner050 <minhyung0237@gmail.com>
2024-08-31 22:58:07 +0900

    Feat: 영화의 영화사 정보 테이블 및 키 매핑 클래스, 레파지토리 삭제.

--- src/main/java/com/cineinfo/v1/repository/kofic/MovieCompaniesRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cineinfo.v1.repository.kofic;
-
-import com.cineinfo.v1.domain.kofic.MovieCompanies;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MovieCompaniesRepository extends JpaRepository<MovieCompanies, String> {
-}
