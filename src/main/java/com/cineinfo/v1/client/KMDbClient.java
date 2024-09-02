7fb367aaad09fc3f9b3c41901cc80641cfc55ad0
moner050 <minhyung0237@gmail.com>
2024-09-02 14:48:07 +0900

    Feat: KMDbClient 클래스 생성 및 기본값 세팅

---
+++ src/main/java/com/cineinfo/v1/client/KMDbClient.java
@@ -0,0 +1,28 @@
+package com.cineinfo.v1.client;
+
+import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class KMDbClient {
+
+    public static String serviceKey;                        // API 서비스 인증키
+    public static String basicUrl;                          // 기본요청 URL
+
+    /******************************** application-kmdb 에 설정된 값 불러오기 ************************************/
+    @Value("${kmdb.serviceKey}")
+    public void setServiceKey(String key) {
+        serviceKey = key;
+    }
+
+    @Value("${kmdb.url}")
+    public void setBasicUrl(String url) {
+        basicUrl = url;
+    }
+
+    /********************************************************************************************************/
+
+
+}
