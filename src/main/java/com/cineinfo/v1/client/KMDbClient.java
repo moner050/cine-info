58a67f2d957e8aaa1701203cb4e00fc82f877c1c
moner050 <minhyung0237@gmail.com>
2024-09-03 00:14:55 +0900

    Feat: KMDb 영화 상세 정보 검색 메서드 생성

--- src/main/java/com/cineinfo/v1/client/KMDbClient.java
+++ src/main/java/com/cineinfo/v1/client/KMDbClient.java
@@ -1,12 +1,21 @@
 package com.cineinfo.v1.client;
 
+import com.cineinfo.v1.dto.kmdb.request.SearchKMDbMovieListReq;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 @Slf4j
 @Component
 public class KMDbClient {
+
+    private final RestTemplate restTemplate;
+
+    public KMDbClient(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 
     public static String serviceKey;                        // API 서비스 인증키
     public static String basicUrl;                          // 기본요청 URL
@@ -24,5 +33,14 @@ public class KMDbClient {
 
     /********************************************************************************************************/
 
+    // 영화 상세 정보 검색
+    public SearchKMDbMovieListRes searchKMDbMovieList (String releaseDts, String releaseDte) {
+        RestTemplateClient restTemplateClient = new RestTemplateClient();
+        SearchKMDbMovieListReq searchKMDbMovieList = new SearchKMDbMovieListReq("500", "kmdb_new2", "Y", "prodYear", releaseDts, releaseDte, "y");
+
+        ParameterizedTypeReference<SearchKMDbMovieListRes> responseType = new ParameterizedTypeReference<SearchKMDbMovieListRes>() {};
+
+        return restTemplateClient.getSearchResponse(responseType, searchKMDbMovieList.toMultiValueMap(), (basicUrl), serviceKey, restTemplate);
+    }
 
 }
