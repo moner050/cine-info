c936cd0718ac4da1d1c50339872d94ce7b606cd3
moner050 <minhyung0237@gmail.com>
2024-09-05 18:13:26 +0900

    Feat: RestTemplateClient 키값 파라미터로 받는 방식 제거. MultiValueMap 에 키값 추가해야 함.

--- src/main/java/com/cineinfo/v1/client/RestTemplateClient.java
+++ src/main/java/com/cineinfo/v1/client/RestTemplateClient.java
@@ -13,16 +13,21 @@ import java.net.URI;
 @Slf4j
 public class RestTemplateClient {
 
-    public <T> T getSearchResponse(ParameterizedTypeReference<T> responseTypeReference, MultiValueMap<String, String> multiValueMap, String url, String key, RestTemplate restTemplate) {
+    public <T> T getSearchResponse(ParameterizedTypeReference<T> responseTypeReference, MultiValueMap<String, String> multiValueMap, String url, RestTemplate restTemplate) {
         URI uri = UriComponentsBuilder.fromUriString(url)
-                .queryParam("key", key)
                 .queryParams(multiValueMap)
                 .build().encode().toUri();
 
         log.info("URI: " + uri);
 
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        // httpEntity 에 headers 의 내용을 담아준다.
+        HttpEntity<Object> httpEntity = new HttpEntity<>(headers);
+
         // RestTemplate 로 결과값을 받아온다.
-        ResponseEntity<T> responseEntity = restTemplate.exchange(uri, HttpMethod.GET, null, responseTypeReference);
+        ResponseEntity<T> responseEntity = restTemplate.exchange(uri, HttpMethod.GET, httpEntity, responseTypeReference);
 
         return responseEntity.getBody();
     }
