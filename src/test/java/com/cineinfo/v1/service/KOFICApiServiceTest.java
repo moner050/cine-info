007febf58b24a5bec366c604f18619dc8f5496a2
moner050 <minhyung0237@gmail.com>
2024-09-06 17:05:12 +0900

    Test: KOFIC 영화, 영화사, 영화인 정보 도메인 및 레파지토리 삭제로 인한 테스트 코드 수정

--- src/test/java/com/cineinfo/v1/service/KOFICApiServiceTest.java
+++ src/test/java/com/cineinfo/v1/service/KOFICApiServiceTest.java
@@ -1,12 +1,8 @@
 package com.cineinfo.v1.service;
 
 import com.cineinfo.v1.domain.kofic.KOFICComCode;
-import com.cineinfo.v1.domain.kofic.KOFICCompanyInfo;
-import com.cineinfo.v1.domain.kofic.KOFICMovieInfo;
 import com.cineinfo.v1.domain.kofic.constant.KOFICSummaryCd;
 import com.cineinfo.v1.repository.kofic.KOFICComCodeRepository;
-import com.cineinfo.v1.repository.kofic.KOFICCompanyInfoRepository;
-import com.cineinfo.v1.repository.kofic.KOFICMovieInfoRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -22,11 +18,7 @@ class KOFICApiServiceTest {
     @Autowired
     KOFICApiService koficApiService;
     @Autowired
-    KOFICMovieInfoRepository KOFICMovieInfoRepository;
-    @Autowired
     KOFICComCodeRepository KOFICComCodeRepository;
-    @Autowired
-    KOFICCompanyInfoRepository KOFICCompanyInfoRepository;
 
     @Test
     @DisplayName("공통코드 저장")
@@ -39,32 +31,6 @@ class KOFICApiServiceTest {
 
         // then
         Assertions.assertThat(KOFICComCodes).isNotNull();
-    }
-
-    @Test
-    @DisplayName("영화 리스트 저장")
-    void saveMovieList() {
-        // given
-        koficApiService.saveMovieList("1", "2024");
-
-        // when
-        List<KOFICMovieInfo> all = KOFICMovieInfoRepository.findAll();
-
-        // then
-        Assertions.assertThat(all.size()).isEqualTo(100);
-    }
-
-    @Test
-    @DisplayName("영화사 리스트 저장")
-    void saveCompanyList() {
-        // given
-        koficApiService.saveCompanyList("1");
-
-        // when
-        List<KOFICCompanyInfo> all = KOFICCompanyInfoRepository.findAll();
-
-        // then
-        Assertions.assertThat(all.size()).isEqualTo(100);
     }
 
 }
