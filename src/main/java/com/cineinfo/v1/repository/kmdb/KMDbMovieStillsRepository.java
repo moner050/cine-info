0a4662429c27feac3fda5774d100a1c4a48f771f
moner050 <minhyung0237@gmail.com>
2024-09-11 22:50:44 +0900

    Feat: KMDbMovieStillsRepository 영화 고유코드로 스틸컷 데이터 찾기 추가

--- src/main/java/com/cineinfo/v1/repository/kmdb/KMDbMovieStillsRepository.java
+++ src/main/java/com/cineinfo/v1/repository/kmdb/KMDbMovieStillsRepository.java
@@ -3,5 +3,9 @@ package com.cineinfo.v1.repository.kmdb;
 import com.cineinfo.v1.domain.kmdb.KMDbMovieStills;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface KMDbMovieStillsRepository extends JpaRepository<KMDbMovieStills, Long> {
+
+    List<KMDbMovieStills> findByKmdbMovieInfo_movieId(String movieId);
 }
