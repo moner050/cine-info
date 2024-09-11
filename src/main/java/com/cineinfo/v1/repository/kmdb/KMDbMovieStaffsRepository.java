3ad6fcf43db095c2981609e893e662b8ffcd8125
moner050 <minhyung0237@gmail.com>
2024-09-11 22:50:28 +0900

    Feat: KMDbMovieStaffsRepository 영화 고유코드와 스태프 이름 or 스태프 크레딧 명칭으로 찾기 추가

--- src/main/java/com/cineinfo/v1/repository/kmdb/KMDbMovieStaffsRepository.java
+++ src/main/java/com/cineinfo/v1/repository/kmdb/KMDbMovieStaffsRepository.java
@@ -2,5 +2,12 @@ package com.cineinfo.v1.repository.kmdb;
 
 import com.cineinfo.v1.domain.kmdb.KMDbMovieStaffs;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface KMDbMovieStaffsRepository extends JpaRepository<KMDbMovieStaffs, Long> {
+
+    Optional<KMDbMovieStaffs> findByKmdbMovieInfo_MovieIdAndStaffNm(String movieId, String StaffNm);
+    List<KMDbMovieStaffs> findByKmdbMovieInfo_MovieIdAndStaffRoleGroup(String movieId, String staffRoleGroup);
 }
