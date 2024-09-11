c575e2e0907c3657cf9939c1e626f5f28333f9ed
moner050 <minhyung0237@gmail.com>
2024-09-11 22:47:21 +0900

    Feat: KMDbMovieInfoRepository 제목, 장르, 키워드 포함된 검색 추가

--- src/main/java/com/cineinfo/v1/repository/kmdb/KMDbMovieInfoRepository.java
+++ src/main/java/com/cineinfo/v1/repository/kmdb/KMDbMovieInfoRepository.java
@@ -3,5 +3,11 @@ package com.cineinfo.v1.repository.kmdb;
 import com.cineinfo.v1.domain.kmdb.KMDbMovieInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface KMDbMovieInfoRepository extends JpaRepository<KMDbMovieInfo, String> {
+
+    List<KMDbMovieInfo> findByTitleContains(String title);
+    List<KMDbMovieInfo> findByGenreContains(String genre);
+    List<KMDbMovieInfo> findByKeywordsContains(String keywords);
 }
