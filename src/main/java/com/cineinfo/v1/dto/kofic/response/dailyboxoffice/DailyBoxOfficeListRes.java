5b3f53bcfd699a06609aae3a5d290241d589f438
moner050 <minhyung0237@gmail.com>
2024-09-17 22:01:31 +0900

    Feat: 일별 / 주간 박스오피스 DTO 클래스에 toEntity 메서드 추가.

--- src/main/java/com/cineinfo/v1/dto/kofic/response/dailyboxoffice/DailyBoxOfficeListRes.java
+++ src/main/java/com/cineinfo/v1/dto/kofic/response/dailyboxoffice/DailyBoxOfficeListRes.java
@@ -1,6 +1,12 @@
 package com.cineinfo.v1.dto.kofic.response.dailyboxoffice;
 
+import com.cineinfo.v1.domain.kmdb.KMDbMovieInfo;
+import com.cineinfo.v1.domain.kofic.KOFICDailyBoxOffice;
+import com.cineinfo.v1.domain.kofic.KOFICDailyBoxOfficeId;
 import lombok.*;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @Getter
 @Setter
@@ -26,4 +32,27 @@ public class DailyBoxOfficeListRes {
     private String audiAcc;
     private String scrnCnt;
     private String showCnt;
+
+    public KOFICDailyBoxOffice toEntity(String repNationCd, String targetDate, KMDbMovieInfo kmDbMovieInfo) {
+        KOFICDailyBoxOfficeId koficDailyBoxOfficeId = new KOFICDailyBoxOfficeId(movieNm, LocalDate.parse(openDt, DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+        return KOFICDailyBoxOffice.builder()
+                .koficDailyBoxOfficeId(koficDailyBoxOfficeId)
+                .movieRank(Integer.parseInt(rank))
+                .rankOldAndNew(rankOldAndNew)
+                .repNationCd(repNationCd)
+                .kmdbMovieInfo(kmDbMovieInfo)
+                .salesAmt(Long.parseLong(salesAmt))
+                .salesShare(Double.parseDouble(salesShare))
+                .salesInten(Long.parseLong(salesInten))
+                .salesChange(Double.parseDouble(salesChange))
+                .salesAcc(Long.parseLong(salesAcc))
+                .audiCnt(Long.parseLong(audiCnt))
+                .audiInten(Long.parseLong(audiInten))
+                .audiChange(Double.parseDouble(audiChange))
+                .audiAcc(Long.parseLong(audiAcc))
+                .scrnCnt(Integer.parseInt(scrnCnt))
+                .showCnt(Integer.parseInt(showCnt))
+                .targetDate(LocalDate.parse(targetDate, DateTimeFormatter.ofPattern("yyyyMMdd")))
+                .build();
+    }
 }
