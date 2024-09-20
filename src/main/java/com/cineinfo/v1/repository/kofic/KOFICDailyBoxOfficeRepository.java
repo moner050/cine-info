d18e19e86411df37d25680abf67b9a21881e1581
moner050 <minhyung0237@gmail.com>
2024-09-20 23:33:46 +0900

    Feat: 일간/주간 박스오피스 순위 데이터에서 영화분류 코드, 영화분류 코드와 기준일자 로 박스오피스 데이터 찾는 메서드 추가

--- src/main/java/com/cineinfo/v1/repository/kofic/KOFICDailyBoxOfficeRepository.java
+++ src/main/java/com/cineinfo/v1/repository/kofic/KOFICDailyBoxOfficeRepository.java
@@ -4,9 +4,12 @@ import com.cineinfo.v1.domain.kofic.KOFICDailyBoxOffice;
 import com.cineinfo.v1.domain.kofic.KOFICDailyBoxOfficeId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface KOFICDailyBoxOfficeRepository extends JpaRepository<KOFICDailyBoxOffice, KOFICDailyBoxOfficeId> {
 
     List<KOFICDailyBoxOffice> findByKmdbMovieInfo_MovieIdIsNull();
+    List<KOFICDailyBoxOffice> findByKoficDailyBoxOfficeId_RepNationCd(String repNationCd);
+    List<KOFICDailyBoxOffice> findByKoficDailyBoxOfficeId_RepNationCdAndTargetDate(String repNationCd, LocalDate targetDate);
 }
--- src/main/java/com/cineinfo/v1/repository/kofic/KOFICWeeklyBoxOfficeRepository.java
+++ src/main/java/com/cineinfo/v1/repository/kofic/KOFICWeeklyBoxOfficeRepository.java
@@ -4,9 +4,12 @@ import com.cineinfo.v1.domain.kofic.KOFICWeeklyBoxOffice;
 import com.cineinfo.v1.domain.kofic.KOFICWeeklyBoxOfficeId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface KOFICWeeklyBoxOfficeRepository extends JpaRepository<KOFICWeeklyBoxOffice, KOFICWeeklyBoxOfficeId> {
 
     List<KOFICWeeklyBoxOffice> findByKmdbMovieInfo_MovieIdIsNull();
+    List<KOFICWeeklyBoxOffice> findByKoficWeeklyBoxOfficeId_RepNationCd(String repNationCd);
+    List<KOFICWeeklyBoxOffice> findByKoficWeeklyBoxOfficeId_RepNationCdAndStartDateRange(String repNationCd, LocalDate startDateRange);
 }
