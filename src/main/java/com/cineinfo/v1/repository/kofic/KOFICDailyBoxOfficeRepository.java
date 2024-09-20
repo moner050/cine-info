d62e9379350f4655be278122057b2dd7a2ec0776
moner050 <minhyung0237@gmail.com>
2024-09-21 02:55:12 +0900

    Feat: 영화분류 코드 및 영화랭킹, 기준일로 데이터 찾는 메서드 추가

--- src/main/java/com/cineinfo/v1/repository/kofic/KOFICDailyBoxOfficeRepository.java
+++ src/main/java/com/cineinfo/v1/repository/kofic/KOFICDailyBoxOfficeRepository.java
@@ -6,9 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 public interface KOFICDailyBoxOfficeRepository extends JpaRepository<KOFICDailyBoxOffice, KOFICDailyBoxOfficeId> {
 
+    Optional<KOFICDailyBoxOffice> findByKoficDailyBoxOfficeId_RepNationCdAndMovieRankAndTargetDate(String repNationCd, Integer movieRank, LocalDate targetDate);
     List<KOFICDailyBoxOffice> findByKmdbMovieInfo_MovieIdIsNull();
     List<KOFICDailyBoxOffice> findByKoficDailyBoxOfficeId_RepNationCd(String repNationCd);
     List<KOFICDailyBoxOffice> findByKoficDailyBoxOfficeId_RepNationCdAndTargetDate(String repNationCd, LocalDate targetDate);
