bf9d9a5ecf1e8590e0bfef5ef1297012b88a9163
moner050 <minhyung0237@gmail.com>
2024-09-27 22:28:12 +0900

    Feat: 주간 박스오피스 순위 PK 값으로 해당 데이터 존재 여부 boolean 타입으로 출력하는 메서드 추가 및 영화분류코드와 영화순위, 시작 기준일로 데이터 찾아오는 메서드 추가

--- src/main/java/com/cineinfo/v1/repository/kofic/KOFICWeeklyBoxOfficeRepository.java
+++ src/main/java/com/cineinfo/v1/repository/kofic/KOFICWeeklyBoxOfficeRepository.java
@@ -6,9 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 public interface KOFICWeeklyBoxOfficeRepository extends JpaRepository<KOFICWeeklyBoxOffice, KOFICWeeklyBoxOfficeId> {
 
+    boolean existsByKoficWeeklyBoxOfficeId_MovieNmAndKoficWeeklyBoxOfficeId_OpenDtAndKoficWeeklyBoxOfficeId_RepNationCdAndStartDateRange(String movieNm, LocalDate openDt, String repNationCd, LocalDate startDateRange);
+    Optional<KOFICWeeklyBoxOffice> findByKoficWeeklyBoxOfficeId_RepNationCdAndMovieRankAndStartDateRange(String repNationCd, Integer movieRank, LocalDate startDateRange);
     List<KOFICWeeklyBoxOffice> findByKmdbMovieInfo_MovieIdIsNull();
     List<KOFICWeeklyBoxOffice> findByKoficWeeklyBoxOfficeId_RepNationCd(String repNationCd);
     List<KOFICWeeklyBoxOffice> findByKoficWeeklyBoxOfficeId_RepNationCdAndStartDateRange(String repNationCd, LocalDate startDateRange);
