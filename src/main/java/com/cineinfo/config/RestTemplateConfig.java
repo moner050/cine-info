f92f452c5c0c3abc8caca3181c792eff47264c49
moner050 <minhyung0237@gmail.com>
2024-09-30 16:03:56 +0900

    Feat: RestTemplate에 Custom Interceptor 적용

--- src/main/java/com/cineinfo/config/RestTemplateConfig.java
+++ src/main/java/com/cineinfo/config/RestTemplateConfig.java
@@ -1,20 +1,27 @@
 package com.cineinfo.config;
 
+import com.cineinfo.v1.interceptor.ResponseBodyModifierInterceptor;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 import java.time.Duration;
 
 @Configuration
+@ComponentScan(basePackages = "com.cineinfo.v1.client")
 public class RestTemplateConfig {
 
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+        // 인터셉터 설정
+        ResponseBodyModifierInterceptor interceptor = new ResponseBodyModifierInterceptor();
+
         return restTemplateBuilder
                 .setConnectTimeout(Duration.ofSeconds(20))
                 .setReadTimeout(Duration.ofSeconds(20))
+                .interceptors(interceptor)
                 .build();
     }
 }
