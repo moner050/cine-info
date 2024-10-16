4fd76520fa52450b5f9d7807a2cfc99215bf0221
moner050 <minhyung0237@gmail.com>
2024-10-16 18:41:10 +0900

    Feat: 일간 박스오피스 순위 recordType DTO 에 새로운 DailyBoxOfficeRequest 를 리턴해주는 of 매서드 추가

--- src/main/java/com/cineinfo/v1/dto/admin/DailyBoxOfficeRequest.java
+++ src/main/java/com/cineinfo/v1/dto/admin/DailyBoxOfficeRequest.java
@@ -2,4 +2,8 @@ package com.cineinfo.v1.dto.admin;
 
 public record DailyBoxOfficeRequest(String startDate, String endDate, String repNationCd) {
 
+    public static DailyBoxOfficeRequest of(String startDate, String endDate, String repNationCd) {
+        return new DailyBoxOfficeRequest(startDate, endDate, repNationCd);
+    }
+
 }
