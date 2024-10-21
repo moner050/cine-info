65c8bb355d8669c45daee51962f997f827dfbfe5
moner050 <minhyung0237@gmail.com>
2024-10-21 15:50:14 +0900

    Feat: Thymeleaf3Properties class -> record 타입으로 변경

--- src/main/java/com/cineinfo/config/ThymeleafConfig.java
+++ src/main/java/com/cineinfo/config/ThymeleafConfig.java
@@ -16,19 +16,12 @@ public class ThymeleafConfig {
             SpringResourceTemplateResolver defaultTemplateResolver,
             Thymeleaf3Properties thymeleaf3Properties
             ) {
-        defaultTemplateResolver.setUseDecoupledLogic(thymeleaf3Properties.isDecoupledLogic());
+        defaultTemplateResolver.setUseDecoupledLogic(thymeleaf3Properties.decoupledLogic());
 
         return defaultTemplateResolver;
     }
 
-    @RequiredArgsConstructor
-    @Getter
     // 유저가 직접 configuration properties 를 만들었을 경우에 스캔을 해줘야 함.
     @ConfigurationProperties("spring.thymeleaf3")
-    public static class Thymeleaf3Properties {
-        /**
-         * Use Thymeleaf 3 Decoupled Logic
-         */
-        private final boolean decoupledLogic;
-    }
+    public record Thymeleaf3Properties(boolean decoupledLogic) { }
 }
